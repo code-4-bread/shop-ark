f4906ed48c517e817aa5c9e41484ca5383f95d50
Code4Bread <iz@kaungmyat.tech>
2022-10-08 17:14:36 +0100

    🚸 Updated to show current level of effect instead of remaining level

--- src/components/Stats/EffectBlock.jsx
+++ src/components/Stats/EffectBlock.jsx
@@ -8,20 +8,13 @@ const EffectBlock = ({ name, value }) => {
   const newValue = value > 15 ? 15 : value;
   const excessValue = value - 15;
 
-  const level = Math.floor(newValue / 5);
-
-  const excessValueDisplay =
-    excessValue < 0 ? `${excessValue}` : `+${excessValue}`;
-
   return (
     <Grid container style={{ marginTop: '5px', color: color }}>
       <Grid item xs={9}>
         <b>{name}:</b>
       </Grid>
       <Grid item xs={3}>
-        <b>
-          {level}/3 {excessValue !== 0 && <b>({excessValueDisplay})</b>}
-        </b>
+        <b>{newValue}/15</b>
       </Grid>
     </Grid>
   );
